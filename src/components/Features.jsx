1016727bd7fce0d1bbac9bc561bc5576897031af
Piyush <87450143+PiyushYadavPY@users.noreply.github.com>
2024-09-20 11:29:17 +0530

    Update Features.jsx
    the className suppose to be screen-max-width instead of screen-max-wdith

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -33,7 +33,7 @@ const Features = () => {
 
   return (
     <section className="h-full common-padding bg-zinc relative overflow-hidden">
-      <div className="screen-max-wdith">
+      <div className="screen-max-width">
         <div className="mb-12 w-full">
           <h1 id="features_title" className="section-heading">Explore the full story.</h1>
         </div>
